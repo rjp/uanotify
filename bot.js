0d7a87c0e0583ee04e17159a03e9fc060f9dc5e3
rjp <zimpenfish@gmail.com>
2010-10-12 07:39:13 +0100

    - move the folder choice to another page - (we might not have them when we access settings.html the first time) - add a link to the folders setting from the profile - bot.js is now spawned by server.js, pass the arguments as JSON - actually cache the UA folder list instead of just printing it - TODO work out how to make this an atomic update - only alert on watched folders - TODO remove hardcoded host/port for the UA server - server.js: add Basic HTTP AUTH - TODO work out how best to handle HTTP AUTH failures - TODO get the authentication details from UA? - server.js: at boot, start up UA sessions for everyone who's active - server.js: lots of jiggling to accomodate HTTP AUTH

--- bot.js
+++ bot.js
@@ -6,7 +6,10 @@ var connect = require('connect');
 var api_keys = require(process.env.HOME + '/.apikeys.js');
 require('./Math.uuid.js');
 
-var notifo_user = process.argv[4];
+var my_json = process.argv[2];
+var my_hash = JSON.parse(my_json);
+
+var notifo_user = my_hash['notify:dest'];
 
 // create this up here
 var notification = new notifo({
@@ -15,7 +18,8 @@ var notification = new notifo({
 });
 
 var r = redis.createClient();
-var username = process.argv[2];
+
+var username = my_hash['ua:user'];
 
 var safe_username = username.replace(/[^A-Za-z0-9]/g, '_');
 
@@ -25,11 +29,10 @@ var unikey = [safe_username, process.pid, token].join(':');
 var nextid = [unikey, 'nextid'].join(':');
 var list = [unikey, 'list', ''].join(':');
 
+
 // start a new list to avoid collisions / race conditions
 function new_list() {
-    sys.puts("incrementing "+nextid);
     notifybot.list = Math.uuid();
-    sys.puts("storing new items in "+notifybot.list);
 }
 
 function buffer_to_strings(x) {
@@ -39,7 +42,7 @@ function buffer_to_strings(x) {
     return x;
 }
 
-function do_notify() {
+function do_notify(x) {
     old_list = notifybot.list
     if (notifo_user == undefined) {
         sys.puts('http://backup.frottage.org:9980/l/'+old_list);
@@ -60,9 +63,8 @@ function notify_list(e, x) {
     buffer_to_strings(x);
     for(i in x) {
         item = JSON.parse(x[i]);
-        sys.puts(sys.inspect(item));
     }
-    do_notify();
+    do_notify(x);
     new_list();
 }
 
@@ -82,7 +84,6 @@ notifybot.shadow = 256;
 
 function extend(v1, v2) {
     for (var property in v2) {
-        sys.puts("copying "+property+" from v2 to v1");
         v1[property] = v2[property];
     }
     return v1;
@@ -103,9 +104,14 @@ function flatten(q, prefix) {
 // <request="folder_list"><searchtype=2/></>
 function reply_folder_list(a) {
 //    <reply="folder_list"><folder=1><name="test"/><accessmode=7/><subtype=1/><unread=1/></><folder=2><name="private"/><accessmode=263/><subtype=1/></><folder=3><name="chat"/><accessmode=7/><subtype=1/><temp=1/></><numfolders=3/></>
+    var f = [];
     for(i in a.children) {
-        sys.puts(sys.inspect(a.children[i]));
+        var v = a.children[i];
+        flatten(v);
+        sys.puts("F "+v.name);
+        f.push(v.name);
     }
+    notifybot.emit('folders', f);
 }
 
 function reply_message_list(a) {
@@ -114,17 +120,20 @@ function reply_message_list(a) {
     flatten(a);
     flatten(x, 'm_');
     extend(a, x);
-    // create a link to the full content of the message as a hash
-    // hopefully this is obscure enough to avoid guessing URLs
-//     md5 = crypto.createHash('md5');
-//     md5.update(JSON.stringify(a));
-//     link = md5.digest('base64');
-    link = Math.uuid();
-    a.link = link;
-    // TODO check this works
-    r.rpush(notifybot.list, JSON.stringify(a), function(){});
-    // TODO check this works
-    r.set(link, JSON.stringify(a), function(){});
+    var auth = my_hash['auth:name'];
+    r.smembers('user:'+auth+':subs', function(err, folders){
+        buffer_to_strings(folders);
+        var q = {}; for(z in folders) { q[folders[z]] = 1 }
+        sys.puts(sys.inspect(q));
+
+        if (q[a.foldername] == 1) {
+            sys.puts("post in a watched folder, "+a.foldername+", from "+a.fromname);
+            link = Math.uuid();
+            a.link = link;
+            r.rpush(notifybot.list, JSON.stringify(a), function(){});
+            r.set(link, JSON.stringify(a), function(){});
+        }
+    });
 }
 
 function announce_message_add(a) {
@@ -133,11 +142,10 @@ function announce_message_add(a) {
 }
 
 function cache_folders(f) {
-    sys.puts("HELLO SIR");
-    sys.puts(sys.inspect(f));
-    r.del('user:zimpenfish:folders', function(){
+    r.del('user:'+my_hash['auth:name']+':folders', function(){
         for(i in f) {
-            r.sadd('user:zimpenfish:folders', f[i], function(){});
+            sys.puts("CF "+f[i]);
+            r.sadd('user:'+my_hash['auth:name']+':folders', f[i], function(){});
         }
     });
 }
@@ -147,6 +155,5 @@ notifybot.addListener("announce_message_add", announce_message_add);
 notifybot.addListener("reply_message_list", reply_message_list);
 notifybot.list = Math.uuid();
 
-sys.puts("storing into "+notifybot.list);
 setInterval(periodic, 1*60*1000);
-notifybot.connect(process.argv[2], process.argv[3], 'ipz.frottage.org', 8080);
+notifybot.connect(my_hash['ua:user'], my_hash['ua:pass'], 'ipz.frottage.org', 8080);
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var uaclient = require('uaclient');
 var notifo = require('notifo');
 var redis = require('redis');
 var connect = require('connect');
+var auth = require('connect-auth');
+var spawn = require('child_process').spawn;
 var api_keys = require(process.env.HOME + '/.apikeys.js');
 
 var r = redis.createClient();
@@ -11,11 +13,31 @@ var r = redis.createClient();
 var server = connect.createServer(
     connect.logger({ format: ':method :url' }),
     connect.bodyDecoder(),
+    auth([
+        auth.Basic({validatePassword: authenticate, realm: 'uanotify'})
+    ]),
     connect.router(app),
     connect.errorHandler({ dumpExceptions: true, showStack: true })
 );
 server.listen(3000);
 console.log('Connect server listening on port 3000');
+
+var ua_sessions = {};
+
+function authenticate(user, pass, success, failure) {
+    sys.puts('pass is '+pass);
+    sys.puts('getting the key auth:'+user);
+    r.get('auth:'+user, function(err, result) {
+        var real_pass = result.toString('utf8');
+        sys.puts('real pass is '+real_pass);
+        if (pass == real_pass) {
+            sys.puts('AUTHENTICATED, PROCEEDING');
+            success();
+        } else {
+            failure();
+        }
+    });
+}
 
 function output_message(req, res, x) {
     a = JSON.parse(x);
@@ -75,28 +97,69 @@ function debuffer_hash(h) {
         h[i] = h[i].toString('utf8');
     }
 }
-function get_user_info(z, callback) {
-        r.hgetall('user:zimpenfish', function(err,x){
-            sys.puts(sys.inspect(x));
-	        debuffer_hash(x);
-            r.smembers('user:zimpenfish:folders', function(err, folders){
-                debuffer_hash(folders);
-                sys.puts(sys.inspect(folders));
-                if (err == undefined) {
-                    // now we need the subscribed folders
-                    r.smembers('user:zimpenfish:subs', function(err, subs){
-                        debuffer_hash(subs);
-                        // convert the array into a hash for quick existence checking
-                        var subhash = {}; for(z in subs) { subhash[subs[z]] = 1; }
-                        sys.puts(sys.inspect(subhash));
-                        if (err == undefined) {
-                            // GRIEF
-                            callback(folders, subhash, x, subs);
-                        }
-                    });
-                }
+
+r.smembers('active:users', function(err, users) {
+    debuffer_hash(users);
+    for(q in users) {
+        get_user_info(users[q], function(folders, subs, profile, sublist) {
+            var b = []; for(z in folders) b.push(z); b.sort();
+            sys.puts("starting a new bot for "+profile['ua:user']+'/'+profile['ua:pass']);
+            profile['auth:name'] = users[q];
+            ua_sessions[auth] = spawn('node', ['bot.js',JSON.stringify(profile)],{cwd:'/Users/rjp/git/uanotify'});
+            // print whatever we get from the bot
+            ua_sessions[auth].stdout.on('data', function(data) {
+                sys.puts("<"+users[q]+"> "+data);
             });
         });
+    }
+});
+
+function get_user_info(auth, callback) {
+    blank_user = { 
+        'ua:user': '', 'ua:pass': '', 'notify:type': 'Notifo', 
+        'notify:dest': '', 'notify:freq': 7200
+    };
+    sys.puts('fetching the hash for user:'+auth);
+    r.sismember('active:users', auth, function(err, isactive) {
+    r.hgetall('user:'+auth, function(err,x){
+        sys.puts(sys.inspect(x));
+        debuffer_hash(x);
+
+        // if we don't have a notify:type, this must be a new user
+        // create one from our blank template and give them no subs
+        // mark them as having no folders for printing in the template
+        if (x == undefined || x['notify:type'] == undefined) {
+            sys.puts("User doesn't exist in the store, creating a blank one");
+            for(z in blank_user) {
+                r.hset('user:'+auth, z, blank_user[z], function(){});
+            }
+            r.del('user:'+auth+':subs', function(){});
+            x = blank_user;
+        }
+        x['active'] = isactive;
+
+        r.smembers('user:'+auth+':folders', function(err, folders){
+            debuffer_hash(folders);
+            sys.puts(sys.inspect(folders));
+            if (err == undefined) {
+                // now we need the subscribed folders
+                r.smembers('user:'+auth+':subs', function(err, subs){
+                    sys.puts('ERROR '+err);
+                    debuffer_hash(subs);
+                    my_subs = []
+                    for (z in subs) { my_subs[z] = subs[z]; }
+                    // convert the array into a hash for quick existence checking
+                    var subhash = {}; for(z in my_subs) { subhash[my_subs[z]] = 1; }
+                    sys.puts(sys.inspect(subhash));
+                    if (err == undefined) {
+                        // GRIEF
+                        callback(folders, subhash, x, my_subs);
+                    }
+                });
+            }
+        });
+    });
+    });
 }
 
 function app(app) {
@@ -116,48 +179,113 @@ function app(app) {
         });
         console.log('return list '+req.params.id)
     });
-    app.get('/profile', function(req,res){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        get_user_info('zimpenfish', function(folders, subs, profile, sublist){
-            jade.renderFile('profile.html', { locals: { profile: profile, folders: folders, subs: subs, sublist: sublist } },
-                function(err, html){ 
-                sys.puts(err);
-                res.end(html); 
+    app.get('/profile', function(req,res,params){
+        req.authenticate(['basic'], function(err, authx){
+            var auth = req.getAuthDetails().user.username;
+            sys.puts('AUTHENTICATED AS '+auth);
+            res.writeHead(200, {'Content-Type':'text/html'});
+            get_user_info(auth, function(folders, subs, profile, sublist){
+                jade.renderFile('profile.html', { locals: { profile: profile, folders: folders, subs: subs, sublist: sublist } },
+                    function(err, html){ 
+                    sys.puts(err);
+                    res.end(html); 
+                });
             });
+        });
+    });
+    app.post('/updatefolders', function(req,res){
+        req.authenticate(['basic'], function(err, authx){
+            var auth = req.getAuthDetails().user.username;
+            r.del('user:'+auth+':subs', function(){
+                for(z in req.body) {
+                    sys.puts("parameter "+z+" = "+req.body[z]);
+                    if (z.substr(0,4) == 'sub_') {
+                        r.sadd('user:'+auth+':subs', req.body[z]);
+                    }
+                }
+            });
+
+            res.writeHead(302, { Location: '/profile' });
+            res.end();
         });
     });
     app.post('/update', function(req,res){
-        var hash = {};
-        hash['ua:user'] = req.body.user;
-        hash['ua:pass'] = req.body.pass;
-        hash['notify:type'] = req.body.type;
-        hash['notify:dest'] = req.body.dest;
-        hash['notify:freq'] = req.body.freq;
-        for(z in hash) {
-            r.hset('user:zimpenfish', z, hash[z], function(){});
-        }
-        r.del('user:zimpenfish:subs', function(){
-            for(z in req.body) {
-                if (z.substr(0,4) == 'sub_') {
-                    r.sadd('user:zimpenfish:subs', z.substr(4));
-                }
+        req.authenticate(['basic'], function(err, authx){
+            var auth = req.getAuthDetails().user.username;
+            var hash = {};
+            hash['ua:user'] = req.body.user;
+            hash['ua:pass'] = req.body.pass;
+            hash['notify:type'] = req.body.type;
+            hash['notify:dest'] = req.body.dest;
+            hash['notify:freq'] = req.body.freq;
+            for(z in hash) {
+                r.hset('user:'+auth, z, hash[z], function(){});
             }
-        });
-        sys.puts(sys.inspect(hash));
-        res.writeHead(302, { Location: '/profile' });
-        res.end();
-    });
-    app.get('/settings', function(req,res){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        // TODO get the user folder subscription somewhere
-        get_user_info('zimpenfish', function(folders, subs, profile, sublist){
-            var b = []; for(z in folders) b.push(z); b.sort();
+            // stop any UA session they have running and start a new one
+            if (ua_sessions[auth]) { 
+                sys.puts("killing old bot session for "+auth);
+                ua_sessions[auth].kill();
+            } 
+            if (req.body.active) {
+                r.sadd('active:users', auth, function(){});
+                sys.puts("spawning a new bot for "+hash['ua:user']+'/'+hash['ua:pass']);
+                hash['auth:name'] = auth;
+                ua_sessions[auth] = spawn('node', ['bot.js',JSON.stringify(hash)],{cwd:'/Users/rjp/git/uanotify'});
+                // print whatever we get from the bot
+                ua_sessions[auth].stdout.on('data', function(data) {
+                    sys.puts("<"+auth+"> "+data);
+                });
+            } else {
+                r.srem('active:users', auth, function(){});
+                sys.puts("not spawning a new bot for "+hash['ua:user']);
+            }
 
-            jade.renderFile('settings.html', { locals: { profile: profile, folders: folders, fkeys: b, subs: subs } },
-                function(err, html){ 
-                sys.puts(err);
-                res.end(html); 
+            sys.puts(sys.inspect(hash));
+            res.writeHead(302, { Location: '/profile' });
+            res.end();
+        });
+    });
+    app.get('/folders', function(req,res){
+        req.authenticate(['basic'], function(err, authx){
+            var auth = req.getAuthDetails().user.username;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            // TODO get the user folder subscription somewhere
+            get_user_info(auth, function(folders, subs, profile, sublist){
+                var b = []; 
+                var safe_folders = {};
+                for(z in folders) {
+                    var q = folders[z];
+                    b.push(q);
+                    safe_folders[q] = q.replace(/[^a-zA-Z0-9]/g, ':')                    
+                    sys.puts("SF "+q+" = "+safe_folders[q]);
+                }
+                b.sort();
+                jade.renderFile('folders.html', { locals: { profile: profile, folders: folders, fkeys: b, subs: subs, safe: safe_folders } },
+                    function(err, html){ 
+                    sys.puts(err);
+                    res.end(html); 
+                });
             });
         });
+    });
+    app.get('/settings', function(req,res){
+        req.authenticate(['basic'], function(err, authx){
+            var auth = req.getAuthDetails().user.username;
+            res.writeHead(200, {'Content-Type':'text/html'});
+            // TODO get the user folder subscription somewhere
+            get_user_info(auth, function(folders, subs, profile, sublist){
+                var b = []; for(z in folders) b.push(z); b.sort();
+
+                jade.renderFile('settings.html', { locals: { profile: profile, folders: folders, fkeys: b, subs: subs } },
+                    function(err, html){ 
+                    sys.puts(err);
+                    res.end(html); 
+                });
+            });
+        });
+    });
+    app.get('/noauth', function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('NOT AUTHENTICATED, BUGGER OFF!');
     });
 }
