a0523f3f325e9205fdb4242affae13946556e76e
rjp <zimpenfish@gmail.com>
2010-11-01 12:30:51 +0000

    - output the body of the message as a nice wrapped text block

--- server.js
+++ server.js
@@ -54,6 +54,25 @@ function authenticate(user, pass, success, failure) {
     });
 }
 
+// stolen from marak's node-mailer
+// http://github.com/Marak/node_mailer/blob/master/lib/node_mailer.js
+function wordwrap(str){
+    var m = 80;
+    var b = "\r\n";
+    var c = false;
+    var i, j, l, s, r;
+    str += '';
+    if (m < 1) {
+      return str;
+    }
+    for (i = -1, l = (r = str.split(/\r\n|\n|\r/)).length; ++i < l; r[i] += s) {
+      for(s = r[i], r[i] = ""; s.length > m; r[i] += s.slice(0, j) + ((s = s.slice(j)).length ? b : "")){
+        j = c == 2 || (j = s.slice(0, m + 1).match(/\S*(\s)?$/))[1] ? m : j.input.length - j[0].length || c == 1 && m || j.input.length + (j = s.slice(m).match(/^\S*/)).input.length;
+      }
+    }
+    return r.join("\n");
+}
+
 function output_message(req, res, x) {
     a = JSON.parse(x);
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -65,7 +84,8 @@ function output_message(req, res, x) {
     summary = a.foldername+'/'+a.message+' ('+a.m_msgpos+'/'+a.nummsgs+') at '+c;
     jade.renderFile('message.html', { locals: {
         summary: summary, from: a.m_fromname, to: to,
-        subject: a.m_subject, body: a.m_text
+        subject: a.m_subject, body: a.m_text,
+        wrapped: wordwrap(a.m_text)
         }}, function(err, html){
         res.end(html);
     });
